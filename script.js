4b5071c1be86b7028e5cce17fe223127981c5257
Michael Baer <michaelbaer@2024-Air.fios-router.home>
2024-06-09 12:07:24 -0400

    updating bias page

--- script.js
+++ script.js
@@ -44,8 +44,9 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    let top = window.scrollY;
+
     sections.forEach(sec => {
-        let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
